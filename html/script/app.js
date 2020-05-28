9f3fb1ac68d0cc89e14f23b8ac94ab83e77e59cb
BaertTorre <torre.baert@student.howest.be>
2020-05-28 17:05:33 +0200

    ultrasone inleze

--- html/script/app.js
+++ html/script/app.js
@@ -6,7 +6,12 @@ const socket = io(`http://${lanIP}`);
 
 const init = function () {
     listenToSocket()
+    setInterval(leesUltrasonicSensors, 100)
 };
+
+const leesUltrasonicSensors = function () {
+    socket.emit('F2B_ultrasonic');
+}
 
 const changeUltrasonicSensor = function (ultrasonicSensor, data){
     const ultrasonicSensorData = document.getElementById(`ultrasonicSensor${ultrasonicSensor}`);
@@ -14,8 +19,14 @@ const changeUltrasonicSensor = function (ultrasonicSensor, data){
 };
 
 const listenToSocket = function () {
-    socket.on("connected", function () {
+    socket.on("connect", function () {                  //connect ontvangt hij standaard bij connectie
         console.log("verbonden met socket webserver");
+    });
+
+    socket.on("B2F_ultrasonic_data", function (ultrasonicData) {
+        for (let i = 0; i < ultrasonicData.length; i++){
+            changeUltrasonicSensor(i, ultrasonicData[i])
+        }
     });
 };
 
